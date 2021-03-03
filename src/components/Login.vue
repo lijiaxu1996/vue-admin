4e3f86cfdf92c6aa3d94549b9b5340e48cf1015f
lijiaxu1996 <lijiaxu1996@gmail.com>
2021-03-04 00:50:16 +0800

    添加图标字体

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,74 @@
 <template>
   <div class="login-container">
-  <video autoplay loop poster="polina.jpg" class="bgvid" id="bgvid" muted="muted">
-          <source src="https://cdn.jsdelivr.net/gh/lijiaxu1996/img-hosting@master/img/me.mp4" type="video/mp4">
+    <video
+      autoplay
+      loop
+      poster="polina.jpg"
+      class="bgvid"
+      id="bgvid"
+      muted="muted"
+    >
+      <source
+        src="https://cdn.jsdelivr.net/gh/lijiaxu1996/img-hosting@master/img/sky.mp4"
+        type="video/mp4"
+      />
     </video>
+    <!-- <div class="login-content">
+      <el-form label-width="0px" class="login-form">
+          <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+          <el-form-item>
+          <el-input></el-input>
+        </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" disabled>登录</el-button>
+            <el-button type="info" disabled>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div> -->
   </div>
 </template>
 <script>
-export default {
-
-};
+export default {};
 </script>
 <style lang="less" scoped>
-body{
-        background-color:gray;
-        background:url("https://cdn.jsdelivr.net/gh/lijiaxu1996/img-hosting@master/img/me.mp4") no-repeat center 0px;
-    }
- video#bgvid {
-        position: fixed; right: 0; bottom: 0;
-        min-width: 100%; min-height: 100%;
-        width: auto; height: auto; z-index: -100;
-        background-size: cover;
-    }
+.login-container {
+  position: relative;
+  height: 100vh;
+  overflow: hidden;
+}
+.login-content {
+  position: absolute;
+  z-index: 100;
+  width: 8rem;
+  height: 10.67rem;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color:transparent;
+}
+.login-content > .login-form{
+  height: 4.44rem;
+  width: 4rem;
+  background-color: rgba(0,0,0,0.3);
+}
+.login-form-item{
+
+}
+video#bgvid {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: -100;
+  background-size: cover;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import {Form,FormItem} from 'element-ui'
-import {Input} from 'element-ui'
+import {Input,Button} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(Element)
 Vue.use(Input)
 Vue.use(Form)
+Vue.use(Button)
 Vue.use(FormItem)
+Vue.use(Element)
+
